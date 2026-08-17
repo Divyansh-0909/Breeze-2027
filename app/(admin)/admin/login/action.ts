dd4879f17ea0c5a86907630249b809698bfa628e
Ekansh <ekanshjain329@gmail.com>
2026-08-17 11:54:00 +0530

    Fixed Build Error

--- app/(admin)/admin/login/action.ts
+++ app/(admin)/admin/login/action.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import { prisma } from '@/lib/prisma'
 import { createClient } from '@/utils/supabase/server'
 
